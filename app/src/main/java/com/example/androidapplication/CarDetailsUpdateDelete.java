101f845cceeeee320419bc6ada802e42e50cb579
ShalaniMW <shalawije@gmail.com>
2020-08-16 19:30:36 +0530

    car details update delete page added

---
+++ app/src/main/java/com/example/androidapplication/CarDetailsUpdateDelete.java
@@ -0,0 +1,14 @@
+package com.example.androidapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CarDetailsUpdateDelete extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_details_update_delete);
+    }
+}
