b06d23c5ae250c62ba9258331258001d3d03b8a3
NISAJA FERNANDO <nisajafernando30@gmail.com>
2020-08-16 23:06:32 +0530

    strings

--- app/src/main/java/com/example/androidapplication/activity_add_notice.java
+++ app/src/main/java/com/example/androidapplication/activity_add_notice.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class RowActivity extends AppCompatActivity {
+public class activity_add_notice extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.row);
+        setContentView(R.layout.activity_add_notice);
     }
 }
