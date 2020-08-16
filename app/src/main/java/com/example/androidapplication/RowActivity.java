5252a155ddb377cc55d5474d92a33e5f3a775a14
NISAJA FERNANDO <nisajafernando30@gmail.com>
2020-08-16 17:28:04 +0530

    added java files

--- app/src/main/java/com/example/androidapplication/RowActivity.java
+++ app/src/main/java/com/example/androidapplication/RowActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class activity_add_notice extends AppCompatActivity {
+public class RowActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_notice);
+        setContentView(R.layout.row);
     }
 }
