d86ed4c3fe5394839541e0014ec3cf76f2c3e17b
TharkanaVish <tharkana.vish@gmail.com>
2020-10-04 23:02:01 +0530

    Added Test cases to BrandNewForm form;Tharkana

--- app/src/androidTest/java/com/example/androidapplication/BrandNewFormTest.java
+++ app/src/androidTest/java/com/example/androidapplication/BrandNewFormTest.java
@@ -1,5 +1,8 @@
 package com.example.androidapplication;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+
 import androidx.test.rule.ActivityTestRule;
 
 import org.junit.After;
@@ -7,6 +10,10 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.junit.Assert.*;
 
 public class BrandNewFormTest {
@@ -15,8 +22,8 @@ public class BrandNewFormTest {
     //Test cases for Brand new form for ordering a new car
     @Rule
     public ActivityTestRule<BrandNewForm> bActivityTestRule = new ActivityTestRule<BrandNewForm>(BrandNewForm.class);
-
     private BrandNewForm bActivity = null;
+    Instrumentation.ActivityMonitor monitor = getInstrumentation().addMonitor(Checkout.class.getName(),null,false);
 
     @Before
     public void setUp() throws Exception {
@@ -26,6 +33,11 @@ public class BrandNewFormTest {
     @Test
     public void testLaunch(){
         assertNotNull(bActivity.findViewById(R.id.checkPay));
+        onView(withId(R.id.checkPay)).perform(click());
+        Activity nextActivity = getInstrumentation().waitForMonitorWithTimeout(monitor,500);
+        assertNotNull(nextActivity);
+
+        nextActivity.finish();
     }
 
     @After
